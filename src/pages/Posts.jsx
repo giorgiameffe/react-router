7039ea0c9f3787a948823de587735edc9e9d979c
giorgiameffe <giorgiameffe03@gmail.com>
2025-04-16 16:44:02 +0200

    feat: install axios and add useState and useEffect

--- src/pages/Posts.jsx
+++ src/pages/Posts.jsx
@@ -1,7 +1,25 @@
+// Hooks
+import { useEffect, useState } from "react";
+// Axios
+import axios from "axios";
+
 // Posts list
+
+const postsEndpoint = 'https://jsonplaceholder.typicode.com/posts';
 
 export default function Posts() {
 
-    return <h2>Posts</h2>
+    const [posts, setPosts] = useState([]);
+
+    useEffect(() => {
+        axios.get(postsEndpoint)
+            .then(res => console.log(res.data))
+    }, [])
+
+    return (
+        <div>
+            <h2>Posts</h2>
+        </div>
+    )
 
 }
