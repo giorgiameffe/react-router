3ec19e77b2be76487a5434524d53a37dbd4398b4
giorgiameffe <giorgiameffe03@gmail.com>
2025-04-16 13:14:07 +0200

    feat: import BrowserRouter, Routes and Route on App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,16 @@
 // React Router
-import { BrowserRouter } from "react-router-dom";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 function App() {
 
   return (
     <>
       <h1>React Blog Pages</h1>
-
+      <BrowserRouter>
+        <Routes>
+          <Route></Route>
+        </Routes>
+      </BrowserRouter>
     </>
   )
 }
